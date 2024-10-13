7317a9f84bd9184c4edeb8643ef110c768956875
lkrenz <liamkrenz100@gmail.com>
2024-10-12 18:56:54 -0700

    Everything works, but testLargest takes slightly too long.

--- src/DNA.java
+++ src/DNA.java
@@ -13,36 +13,35 @@ import java.util.ArrayList;
  */
 
 public class DNA {
-
-    /**
-     * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
-     */
+    
     public static int STRCount(String sequence, String STR) {
-
         ArrayList<Integer> sequences = new ArrayList<>();
         int max = 0;
         int strLength = STR.length();
+        boolean continueSequence = false;
 
-        for (int i = 0; i  < sequence.length(); i++) {
-            int j = 0;
-            while (j >= 0 && j < sequences.size()) {
-                if (STR.charAt(sequences.get(j) % strLength) == sequence.charAt(i)) {
-                    sequences.set(j, sequences.get(j) + 1);
+        for (int i = 0; i < sequence.length(); i++) {
+            for (int j = 0; j < sequences.size(); j++) {
+                int index = sequences.get(j);
+                if (STR.charAt(index % strLength) == sequence.charAt(i)) {
+                    sequences.set(j, index + 1);
+                    if (index % strLength == 0) {
+                        continueSequence = true;
+                    }
                 }
-                else {
-                    int length = sequences.get(j) / strLength;
+                else { // This could be making the code pass over if the first sequence ends
+                    int length = index / strLength;
                     if (length > max) {
                         max = length;
                     }
                     sequences.remove(j);
                     j--;
-                    if (j < 0) {
+                    if (sequences.isEmpty() && j < 0) {
                         break;
                     }
                 }
-                j++;
             }
-            if (sequence.charAt(i) == STR.charAt(0)) {
+            if (!continueSequence && sequence.charAt(i) == STR.charAt(0)) {
                 sequences.add(1);
             }
         }
