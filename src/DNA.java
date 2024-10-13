b555c2b9421d8671940575b3119163cb0398ec90
lkrenz <liamkrenz100@gmail.com>
2024-10-12 19:29:31 -0700

    Everything works, but testLargest takes slightly too long. Also fixed bug in last commit.

--- src/DNA.java
+++ src/DNA.java
@@ -13,23 +13,27 @@ import java.util.ArrayList;
  */
 
 public class DNA {
-    
+
+
     public static int STRCount(String sequence, String STR) {
         ArrayList<Integer> sequences = new ArrayList<>();
         int max = 0;
         int strLength = STR.length();
-        boolean continueSequence = false;
+        boolean continueSequence;
 
         for (int i = 0; i < sequence.length(); i++) {
+            continueSequence = false;
+            char currentChar = sequence.charAt(i);
+
             for (int j = 0; j < sequences.size(); j++) {
                 int index = sequences.get(j);
-                if (STR.charAt(index % strLength) == sequence.charAt(i)) {
+                if (STR.charAt(index % strLength) == currentChar) {
                     sequences.set(j, index + 1);
                     if (index % strLength == 0) {
                         continueSequence = true;
                     }
                 }
-                else { // This could be making the code pass over if the first sequence ends
+                else {
                     int length = index / strLength;
                     if (length > max) {
                         max = length;
@@ -41,7 +45,7 @@ public class DNA {
                     }
                 }
             }
-            if (!continueSequence && sequence.charAt(i) == STR.charAt(0)) {
+            if (!continueSequence && currentChar == STR.charAt(0)) {
                 sequences.add(1);
             }
         }
