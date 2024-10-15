f707d3912b380bbcb033c41b1fb7854499ea3f6a
lkrenz <liamkrenz100@gmail.com>
2024-10-15 16:20:14 -0700

    Started hash solution, need to add index out of bounds checks.

--- src/DNA.java
+++ src/DNA.java
@@ -15,59 +15,105 @@ import java.util.ArrayList;
 public class DNA {
 
     public static int STRCount(String sequence, String STR) {
+//
+//        // Any value that will be calculated more than once is calculated before iteration
+//        ArrayList<Integer> sequences = new ArrayList<>();
+//        int max = 0;
+//        int strLength = STR.length();
+//        boolean continueSequence;
+//        char[] strArray = STR.toCharArray();
+//        char startChar = STR.charAt(0);
+//        int sequenceLength = sequence.length();
+//
+//        // Iterates through every letter in the sequence
+//        for (int i = 0; i < sequenceLength; i++) {
+//            continueSequence = false;
+//            char currentChar = sequence.charAt(i);
+//
+//            // Goes through every sequence
+//            for (int j = 0; j < sequences.size(); j++) {
+//                int index = sequences.get(j);
+//
+//                // If the current letter matches the next letter in a given sequence, it is incremented
+//                if (strArray[index % strLength] == currentChar) {
+//                    sequences.set(j, index + 1);
+//                    if (index % strLength == 0) {
+//                        continueSequence = true;
+//                    }
+//                }
+//                else {
+//                    // If the current letter doesn't match, the sequence is terminated and totalled
+//                    int length = index / strLength;
+//                    if (length > max) {
+//                        max = length;
+//                    }
+//                    sequences.remove(j); // I might want to move away from arraylists, this is linear time
+//                    j--;
+//                    if (sequences.isEmpty()) {
+//                        break;
+//                    }
+//                }
+//            }
+//            // Adds a new sequence if the current letter is the start letter and it wouldn't repeat another sequence.
+//            if (!continueSequence && currentChar == startChar) {
+//                sequences.add(1);
+//            }
+//        }
+//
+//        // Iterates through any sequences left at the end and totals them.
+//        if (!sequences.isEmpty()) {
+//            for (Integer length : sequences) {
+//                if (length / strLength > max) {
+//                    max = length / strLength;
+//                }
+//            }
+//        }
+//        return max;
 
-        // Any value that will be calculated more than once is calculated before iteration
-        ArrayList<Integer> sequences = new ArrayList<>();
+        int sequenceLength = sequence.length();
+        long strHash = hashFunction(STR);
+        long sequenceHash = hashFunction(sequence.substring(0, STR.length()));
+        int radix = 116;
+        long p = 2147483647;
+        int[] map = createArray();
         int max = 0;
-        int strLength = STR.length();
-        boolean continueSequence;
-        char[] strArray = STR.toCharArray();
-        char startChar = STR.charAt(0);
+        long radixPower = (long) Math.pow(radix, STR.length() - 1);
 
-
-        // Iterates through every letter in the sequence
-        for (int i = 0; i < sequence.length(); i++) {
-            continueSequence = false;
-            char currentChar = sequence.charAt(i);
-
-            // Goes through every sequence
-            for (int j = 0; j < sequences.size(); j++) {
-                int index = sequences.get(j);
-
-                // If the current letter matches the next letter in a given sequence, it is incremented
-                if (strArray[index % strLength] == currentChar) {
-                    sequences.set(j, index + 1);
-                    if (index % strLength == 0) {
-                        continueSequence = true;
+        for (int i = 0; i < sequenceLength; i++) {
+            if (strHash == sequenceHash) {
+                int j = i;
+                int localMax = 0;
+                while (true) {
+                    if (hashFunction(sequence.substring(i, i + STR.length())) == strHash) {
+                        j += STR.length();
+                        localMax++;
+                        continue;
                     }
+                    break;
                 }
-                else {
-                    // If the current letter doesn't match, the sequence is terminated and totalled
-                    int length = index / strLength;
-                    if (length > max) {
-                        max = length;
-                    }
-                    sequences.remove(j); // I might want to move away from arraylists, this is linear time
-                    j--;
-                    if (sequences.isEmpty()) {
-                        break;
-                    }
-                }
+                i = j;
             }
-            // Adds a new sequence if the current letter is the start letter and it wouldn't repeat another sequence.
-            if (!continueSequence && currentChar == startChar) {
-                sequences.add(1);
-            }
+            sequenceHash = ((sequenceHash + p) - (sequence.charAt(i) * radixPower) % p) % p;
+            sequenceHash = ((sequenceHash * radix) + sequence.charAt(i + STR.length())) % p;
         }
 
-        // Iterates through any sequences left at the end and totals them.
-        if (!sequences.isEmpty()) {
-            for (Integer length : sequences) {
-                if (length / strLength > max) {
-                    max = length / strLength;
-                }
-            }
+    }
+
+
+    public static long hashFunction(String str) {
+        long hash = 0;
+        for (int i = 0; i < str.length(); i++) {
+            hash = (hash * 256 + str.charAt(i)) % 2147483647;
         }
-        return max;
+        return hash;
+    }
+
+    public static int[] createArray() {
+        int[] array = new int[117];
+        array['t'] = 4;
+        array['g'] = 3;
+        array['c'] = 2;
+        array['a'] = 1;
+        return array;
     }
 }
